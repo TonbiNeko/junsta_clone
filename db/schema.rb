9326c0ad19c922d5153df35e2eed571577b0ce3d
Junko Mikami <junkomikami312nz@gmail.com>
2020-12-30 20:20:26 +0900

    add unique constraints to Users table
    ユニーク制約をuserテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_083412) do
+ActiveRecord::Schema.define(version: 2020_12_30_111515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_083412) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
