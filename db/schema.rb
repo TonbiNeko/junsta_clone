467ac9fbc963c8987b95cc405be6981a54b6acfd
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-05 17:25:00 +0900

    change column name of favorite table
    favoriteテーブルのカラム名の変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_081804) do
+ActiveRecord::Schema.define(version: 2021_01_05_082215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "blog_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
