0c1140abb32b51cc33968117a15b91dc021b8e98
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-05 17:19:27 +0900

    create favorite table
    favoriteテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_111515) do
+ActiveRecord::Schema.define(version: 2021_01_05_081804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
