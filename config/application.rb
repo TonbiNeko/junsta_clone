2b50c1efcad9b171c52c6d0e6c85e7c493995655
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-07 21:00:40 +0900

    change time zone to TOKYO
    タイムゾーンを東京に変更

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module JunstaClone
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     
     #config.load_defaults 5.2
@@ -18,5 +20,6 @@ module JunstaClone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    end
   end
 end
