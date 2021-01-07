e4cd510916f248b65edf036317322ea7b19f9654
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-07 16:34:56 +0900

    お気に入りページを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @favorites = current_user.favorites
+  end
+
   def new
     @user = User.new
   end
